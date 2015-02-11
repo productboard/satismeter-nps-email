0a3f6da035d87353651fc15593e0efb79d2a1076
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2015-02-11 12:51:37 +0100

    initial commit

---
+++ examples/example.js
@@ -0,0 +1,10 @@
+var render = require('..');
+
+document.body.innerHTML = render({
+  intro: 'Hi!\n\nPlease fill in the survey below:',
+  outro: 'Bye!',
+  visitor: {id: '1'},
+  url: 'http://localhost/survey',
+  token: 'aaa',
+  language: 'de'
+});
---
+++ index.js
@@ -0,0 +1,137 @@
+var marked = require('marked');
+var h = require('hyperscript');
+var range = require('range');
+var translations = require('nps-translations');
+var Uri = require('jsuri');
+var is = require('is');
+
+function ratingUrl(url, token, visitor, rating) {
+  if (!url) {
+    return null;
+  }
+  var uri = new Uri(url);
+  uri.addQueryParam('token', token);
+  uri.addQueryParam('id', visitor.source_id);
+  if (visitor.email) {
+    uri.addQueryParam('email', visitor.email);
+  }
+  if (visitor.name) {
+    uri.addQueryParam('name', visitor.name);
+  }
+  if (is.number(rating)) {
+    uri.addQueryParam('rating', rating.toString());
+  }
+  return uri.toString();
+}
+
+
+function scaleItem(url, token, visitor, rating) {
+  var attrs = {style: {
+      'background-color': '#FF9500',
+      'border-radius': '50%',
+      color: 'white',
+      display: 'inline-block',
+      height: '40px',
+      'line-height': '40px',
+      margin: '2px',
+      'margin-left': rating === 0 ? 0 : undefined,
+      'margin-right': rating === 10 ? 0 : undefined,
+      'text-align': 'center',
+      'text-decoration': 'none',
+      width: '40px'
+  }};
+  var url = ratingUrl(url, token, visitor, rating);
+  if (url) {
+    attrs.href = url;
+  }
+  return h('a', attrs, rating);
+}
+
+function scale(url, token, visitor) {
+  return h('div', {
+      style: {
+        width: (11 * 40 + 10 * 4) + 'px'
+      }
+    },
+    range(0, 11).map(function(rating) {
+      return scaleItem(url, token, visitor, rating);
+    })
+  );
+}
+
+function legend(language) {
+  return h('div', {
+      style: {
+        color: '#999',
+        'font-size': '12px',
+        'font-style': 'italic',
+        'margin-bottom': '30px',
+        width: (11 * 40 + 10 * 4) + 'px'
+      }
+    },
+    h('div', {style: {
+      float: 'left'
+    }}, translations[language].NOT_AT_ALL),
+    h('div', {style: {
+      float: 'right'
+    }}, translations[language].EXTREMLY),
+    h('div', {style: {
+      clear: 'both'
+    }})
+  );
+}
+
+function rating(url, token, visitor, language) {
+  return [scale(url, token, visitor), legend(language)];
+}
+
+function n(html) {
+  var node = document.createElement('div');
+  node.innerHTML = html;
+  return node;
+}
+
+function poweredBy() {
+  return [
+    h('hr', {style: {
+      border: 'none',
+      'border-top': '1px solid #eee'
+    }}),
+    h('span', 'Powered by ',
+      h('a', {
+        href: 'https://satismeter.com',
+        style: {
+          color: '#ff4981',
+          'text-decoration': 'none'
+        }
+      }, 'SatisMeter')
+    )
+  ];
+}
+
+function render(options) {
+  var intro = options.intro;
+  var outro = options.outro;
+  var token = options.token;
+  var url = options.url;
+  var visitor = options.visitor || {};
+  var language = options.language || 'en';
+  var color = options.color || 'pink';
+
+  return h('div', {
+    style: {
+      'font-family': 'sans-serif',
+      'line-height': 1.6,
+      'font-size': '16px',
+      margin: 0,
+      padding: 0
+    }
+  }, [
+    n(marked(options.intro)),
+    rating(url, token, visitor, language),
+    n(marked(options.outro)),
+    poweredBy()
+  ]).outerHTML;
+}
+
+module.exports = render;
