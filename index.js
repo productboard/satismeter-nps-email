42643046555d384facf7e856d79773584427e125
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2016-12-09 11:03:27 +0100

    add inline template

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var transform = require('./transform');
 
 var TEMPLATES = {
   default: handlebars.compile(fs.readFileSync(__dirname + '/survey.hbs', 'utf8')),
+  inline: handlebars.compile(fs.readFileSync(__dirname + '/inline.hbs', 'utf8')),
   zonky: handlebars.compile(fs.readFileSync(__dirname + '/zonky-survey.hbs', 'utf8'))
 };
 
