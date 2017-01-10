b099b99d71954d6b73bf107a491f6a65dc7c1b19
Jakub Sedlacek <sedlacek.jakub@gmail.com>
2017-01-10 14:09:11 +0100

    ilos template

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var transform = require('./transform');
 var TEMPLATES = {
   default: handlebars.compile(fs.readFileSync(__dirname + '/survey.hbs', 'utf8')),
   inline: handlebars.compile(fs.readFileSync(__dirname + '/inline.hbs', 'utf8')),
-  zonky: handlebars.compile(fs.readFileSync(__dirname + '/zonky-survey.hbs', 'utf8'))
+  zonky: handlebars.compile(fs.readFileSync(__dirname + '/zonky-survey.hbs', 'utf8')),
+  ilos: handlebars.compile(fs.readFileSync(__dirname + '/ilos-survey.hbs', 'utf8'))
 };
 
 function render(options) {
