0fa5213ccbc7a52ed97b1ef574a9aa58b071f22c
Alberto Casares <better.acs@gmail.com>
2022-09-13 15:29:02 +0200

    Escaping double quotes

--- src/transformV2.ts
+++ src/transformV2.ts
@@ -130,7 +130,8 @@ export function transformV2(options: TransformV2Options): TemplateV2Options {
       }
     }
 
-    return new SafeString(uri.toString());
+    const escaped = uri.toString().replace('"', '&quot;');
+    return new SafeString(escaped);
   }
 
   const renderer = new marked.Renderer();
